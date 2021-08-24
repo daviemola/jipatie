8a7055252d64421b69d884c03382d16faf05709a
david mola <daviemola@gmail.com>
2021-08-25 02:50:18 +0530

    miss google auth secret changed

--- config/index.js
+++ config/index.js
@@ -6,6 +6,3 @@ export const NEXT_URL =
 
 export const PER_PAGE = 8
 export const DASH_PER_PAGE = 10
-export const GOOGLE_CLIENT_ID =
-  '898831955690-qq7ol724l5t7ft4pdlsk0ds249lkr6a8.apps.googleusercontent.com'
-export const GOOGLE_CLIENT_SECRET = '5q0rq6gt2tnn7_iQYm8mCNRk'
