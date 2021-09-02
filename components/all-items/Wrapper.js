ef7d18f7cd821f8fcde6b71601d50c0945c8a3c1
david mola <daviemola@gmail.com>
2021-09-03 00:23:35 +0530

    height bug solved

--- components/all-items/Wrapper.js
+++ components/all-items/Wrapper.js
@@ -2,7 +2,7 @@ function wrapper({ children, title }) {
   let desc =
     'Feel free to request any item listed here from the owners. If request is accepted you will be notified.'
   return (
-    <section className="text-gray-600 pt-8 h-4/5">
+    <section className="text-gray-600 pt-8 min-h-75/100">
       <div className="px-10 mx-auto max-w-7xl sm:px-6">
         <h1 className="text-2xl font-semibold text-center dark:text-gray-50">
           {title}
--- components/dashboard/Wrapper.js
+++ components/dashboard/Wrapper.js
@@ -1,6 +1,6 @@
 export default function Wrapper({ children, title }) {
   return (
-    <section className="text-gray-600 h-4/5 overflow-hidden">
+    <section className="text-gray-600 min-h-75/100 overflow-hidden">
       <div className="container px-3 py-6 mx-auto max-w-screen-xl">
         <div className="container mx-auto px-4 sm:px-8 max-w-3xl">
           <h1 className="sm:text-xl text-lg md:text-xl py-2 mb-3 tracking-wide uppercase font-semibold text-gray-600">
